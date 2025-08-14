3dde8a4e86efa19a19aae3be8edd4b2cfb6d9365
nicof <nico.llousas@gmail.com>
2025-08-14 18:49:09 -0300

    Spring Initializr

---
+++ src/test/java/com/example/uade/tpo/Felsani/Motors/FelsaniMotorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uade.tpo.Felsani.Motors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FelsaniMotorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
